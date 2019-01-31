a1a22897920f83787a8c47702e202f61e6d212f2
Umputun <umputun@gmail.com>
2019-01-30 18:57:14 -0600

    extend bw with func input

--- blackwords_test.go
+++ blackwords_test.go
@@ -17,6 +17,7 @@ func TestBlackwords(t *testing.T) {
 		inp  string
 		code int
 	}{
+		{"", 200},
 		{"blah blah body", 200},
 		{"blah blah body bad1", 403},
 		{"blah blah body bad", 200},
@@ -32,13 +33,50 @@ func TestBlackwords(t *testing.T) {
 
 	client := http.Client{Timeout: 5 * time.Second}
 
-	for _, tt := range tbl {
-		req, err := http.NewRequest("GET", u, bytes.NewBuffer([]byte(tt.inp)))
-		assert.Nil(t, err)
+	for n, tt := range tbl {
+		t.Run(fmt.Sprintf("test-%d", n), func(t *testing.T) {
+			req, err := http.NewRequest("GET", u, bytes.NewBuffer([]byte(tt.inp)))
+			assert.Nil(t, err)
 
-		r, err := client.Do(req)
-		assert.Nil(t, err)
-		assert.Equal(t, tt.code, r.StatusCode)
+			r, err := client.Do(req)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.code, r.StatusCode)
+		})
+	}
+}
+
+func TestBlackwordsFn(t *testing.T) {
+	tbl := []struct {
+		inp  string
+		code int
+	}{
+		{"", 200},
+		{"blah blah body", 200},
+		{"blah blah body bad1", 403},
+		{"blah blah body bad", 200},
+		{"blah bad2 body bad", 403},
+		{`{"fld": 123, "aa": {"$where": {"aaa": 567}}}`, 403},
 	}
 
+	bwMiddleware := BlackWordsFn(func() []string {
+		return []string{"bad1", "bad2", "$where"}
+	})
+
+	ts := httptest.NewServer(bwMiddleware(getTestHandlerBlah()))
+	defer ts.Close()
+
+	u := fmt.Sprintf("%s%s", ts.URL, "/something")
+
+	client := http.Client{Timeout: 5 * time.Second}
+
+	for n, tt := range tbl {
+		t.Run(fmt.Sprintf("test-%d", n), func(t *testing.T) {
+			req, err := http.NewRequest("GET", u, bytes.NewBuffer([]byte(tt.inp)))
+			assert.Nil(t, err)
+
+			r, err := client.Do(req)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.code, r.StatusCode)
+		})
+	}
 }
