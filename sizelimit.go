0a3e9968c2c5177024a28675746111921f264bf7
Umputun <umputun@gmail.com>
2019-02-21 20:30:14 -0600

    add middleware for body size limiting

---
+++ sizelimit.go
@@ -0,0 +1,40 @@
+package rest
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+// SizeLimit middleware checks if body size is above the limit and returns StatusRequestEntityTooLarge (413)
+func SizeLimit(size int64) func(http.Handler) http.Handler {
+
+	return func(h http.Handler) http.Handler {
+
+		fn := func(w http.ResponseWriter, r *http.Request) {
+
+			// check ContentLength
+			if r.ContentLength > size {
+				w.WriteHeader(http.StatusRequestEntityTooLarge)
+				return
+			}
+
+			// check size of the actual body
+			content, err := ioutil.ReadAll(io.LimitReader(r.Body, size))
+			if err != nil {
+				w.WriteHeader(http.StatusServiceUnavailable)
+				return
+			}
+			if len(content) >= int(size) {
+				w.WriteHeader(http.StatusRequestEntityTooLarge)
+				return
+			}
+			r.Body = ioutil.NopCloser(bytes.NewReader(content))
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		return http.HandlerFunc(fn)
+	}
+}
---
+++ sizelimit_test.go
@@ -0,0 +1,59 @@
+package rest
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSizeLimit(t *testing.T) {
+
+	tbl := []struct {
+		method string
+		body   string
+		code   int
+	}{
+		{"GET", "", 200},
+		{"POST", "1234567", 200},
+		{"POST", "12345678901", 413},
+		{"POST", "1234567", 200},
+	}
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		require.NoError(t, err, "body read failed")
+		_, err = w.Write(body)
+		require.NoError(t, err, "body write failed")
+		dump, _ := httputil.DumpRequest(r, true)
+		t.Log(string(dump))
+	})
+
+	ts := httptest.NewServer(SizeLimit(10)(handler))
+	defer ts.Close()
+
+	for i, tt := range tbl {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			client := http.Client{Timeout: 1 * time.Second}
+			req, err := http.NewRequest(tt.method, ts.URL+"/"+strconv.Itoa(i), strings.NewReader(tt.body))
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			require.Equal(t, tt.code, resp.StatusCode)
+
+			if resp.StatusCode != http.StatusRequestEntityTooLarge {
+				body, err := ioutil.ReadAll(resp.Body)
+				require.NoError(t, err)
+				defer resp.Body.Close()
+				assert.Equal(t, tt.body, string(body), "body match")
+			}
+		})
+	}
+}
