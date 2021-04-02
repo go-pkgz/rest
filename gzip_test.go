5530f4678b6f541dd5233ebb699fc1e0c234e8e1
Umputun <umputun@gmail.com>
2021-04-02 13:01:45 -0500

    lint: spelling

--- gzip_test.go
+++ gzip_test.go
@@ -18,7 +18,7 @@ func TestGzip(t *testing.T) {
 		_, err := w.Write([]byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
 			"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took " +
 			"a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries," +
-			" but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised" +
+			" but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized" +
 			" in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " +
 			"and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."))
 		require.NoError(t, err)
