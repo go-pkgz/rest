78d77b66beba656bd0d7c0aaff4dc2537f4d101f
Umputun <umputun@gmail.com>
2021-04-04 02:35:58 -0500

    check gz headers and allow custom headers

--- gzip_test.go
+++ gzip_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGzip(t *testing.T) {
+func TestGzipCustom(t *testing.T) {
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
 			"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took " +
@@ -23,7 +23,7 @@ func TestGzip(t *testing.T) {
 			"and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."))
 		require.NoError(t, err)
 	})
-	ts := httptest.NewServer(Gzip(handler))
+	ts := httptest.NewServer(Gzip("text/plain", "text/html")(handler))
 	defer ts.Close()
 
 	client := http.Client{}
@@ -32,6 +32,7 @@ func TestGzip(t *testing.T) {
 		req, err := http.NewRequest("GET", ts.URL+"/something", nil)
 		require.NoError(t, err)
 		req.Header.Set("Accept-Encoding", "gzip")
+		req.Header.Set("Content-Type", "text/plain")
 		resp, err := client.Do(req)
 		require.NoError(t, err)
 		assert.Equal(t, 200, resp.StatusCode)
@@ -46,6 +47,58 @@ func TestGzip(t *testing.T) {
 		require.NoError(t, err)
 		assert.True(t, strings.HasPrefix(string(b), "Lorem Ipsum"), string(b))
 	}
+
+	{
+		req, err := http.NewRequest("GET", ts.URL+"/something", nil)
+		require.NoError(t, err)
+		req.Header.Set("Accept-Encoding", "gzip")
+		req.Header.Set("Content-Type", "something")
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+		assert.Equal(t, 200, resp.StatusCode)
+		defer resp.Body.Close()
+		b, err := ioutil.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, 576, len(b), "uncompressed size")
+	}
+
+}
+
+func TestGzipDefault(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+			"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took " +
+			"a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries," +
+			" but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized" +
+			" in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " +
+			"and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."))
+		require.NoError(t, err)
+	})
+	ts := httptest.NewServer(Gzip()(handler))
+	defer ts.Close()
+
+	client := http.Client{}
+
+	{
+		req, err := http.NewRequest("GET", ts.URL+"/something", nil)
+		require.NoError(t, err)
+		req.Header.Set("Accept-Encoding", "gzip")
+		req.Header.Set("Content-Type", "text/plain")
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+		assert.Equal(t, 200, resp.StatusCode)
+		defer resp.Body.Close()
+		b, err := ioutil.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, 357, len(b), "compressed size")
+
+		gzr, err := gzip.NewReader(bytes.NewBuffer(b))
+		require.NoError(t, err)
+		b, err = ioutil.ReadAll(gzr)
+		require.NoError(t, err)
+		assert.True(t, strings.HasPrefix(string(b), "Lorem Ipsum"), string(b))
+	}
+
 	{
 		req, err := http.NewRequest("GET", ts.URL+"/something", nil)
 		require.NoError(t, err)
@@ -56,7 +109,20 @@ func TestGzip(t *testing.T) {
 		b, err := ioutil.ReadAll(resp.Body)
 		assert.NoError(t, err)
 		assert.Equal(t, 576, len(b), "uncompressed size")
+	}
 
+	{
+		req, err := http.NewRequest("GET", ts.URL+"/something", nil)
+		require.NoError(t, err)
+		req.Header.Set("Accept-Encoding", "gzip")
+		req.Header.Set("Content-Type", "something")
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+		assert.Equal(t, 200, resp.StatusCode)
+		defer resp.Body.Close()
+		b, err := ioutil.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, 576, len(b), "uncompressed size")
 	}
 
 }
